1c7ea4bc30f5ca47352ca9a2847affdd0716c07d
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-29 17:05:36 +0530

    renders the App

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import {
   Route,
 } from "react-router-dom";
 import App from "./App";
-
+import Invoices from "./routes/invoices";
+import Expenses from "./routes/expenses";
 
 const rootElement = document.getElementById("root");
 render(
       <BrowserRouter>
-        <App />
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+       
       </BrowserRouter>, 
       rootElement
 );
