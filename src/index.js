67085febaae9a14d174667682a8f7791fb2b3f0b
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-29 21:35:08 +0530

    added URL param to match url for same pattern

--- src/index.js
+++ src/index.js
@@ -7,14 +7,17 @@ import {
 import App from "./App";
 import Invoices from "./routes/invoices";
 import Expenses from "./routes/expenses";
+import Invoice from "./routes/invoice";
 
 const rootElement = document.getElementById("root");
 render(
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />}>
-          <Route path="expenses" element={<Expenses />} />
-          <Route path="invoices" element={<Invoices />} />
+            <Route path="/" element={<App />}>
+            <Route path="expenses" element={<Expenses />} />
+            <Route path="invoices" element={<Invoices />} >
+              <Route path =":invoiceId" element={<Invoice />} />
+            </Route>
           <Route 
             path="*"
             element={
