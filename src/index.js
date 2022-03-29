4607ea5bb1234076aa26513b204aa734982f5abf
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-29 17:00:08 +0530

    imported Routes, route from react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import {render} from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { 
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import App from "./App";
 
 
